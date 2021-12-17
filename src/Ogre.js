309b885fd3ac44a880b903c865983a1bf2fd9b24
CStirnem <cedric.stirnemann@hotmail.fr>
2021-12-17 10:02:24 +0100

    optimized ogre AI

--- src/Ogre.js
+++ src/Ogre.js
@@ -4,7 +4,7 @@ const AGGRO_RANGE = 600;
 const CLOSE_RANGE = 10;
 
 const SPEED = 100;
-const JUMP_STRENGTH = 300;
+const JUMP_STRENGTH = 200;
 
 export default class Ogre extends Monster
 {
@@ -20,38 +20,39 @@ export default class Ogre extends Monster
     update()
     {
 		if (this.idle) {
-		if (this.direction == 1) {
-			this.play("idleROgre", true);
-			if (this.x < this.player.x) this.idle = false;
-			else return;
-		} else {
-			this.play("idleLOgre", true);
-			if (this.player.x < this.x) this.idle = false;
-			else return;
-		}
-		this.setVelocityX(0);
-
-		if (!this.idle) this.prevX = -1;
-	}
-
-		if (this.x < this.player.x) this.direction = 'R';
-		else this.direction = 'L';
-
-		if (Math.abs(this.player.x - this.x) > AGGRO_RANGE || Math.abs(this.player.x - this.x) < CLOSE_RANGE) {		// If out of aggro range
-			if (this.direction == 1) this.play("idleROgre", true);
-			else this.play("idleLOgre", true);
+			if (this.direction == 1) {
+				this.play("idleROgre", true);
+				if (this.x > this.player.x) this.idle = false;
+				console.log("!!!");
+			} else {
+				this.play("idleLOgre", true);
+				if (this.player.x > this.x) this.idle = false;
+			}
 			this.setVelocityX(0);
-		} else {
-			if (this.direction == 1) this.play("runROgre", true);
-			else this.play("runLOgre", true);
-			this.setVelocityX(SPEED * this.direction);
 
-			if (this.body.onFloor()) {
-				if ((this.direction == 1 && this.body.blocked.right) || (this.direction == -1 && this.body.blocked.left)) {
-					if (this.prevX == this.x) this.idle = true;		// if in front of too high wall
-					else {
-						this.setVelocityY(-JUMP_STRENGTH);
-						this.prevX = this.x;
+			if (!this.idle) this.prevX = -1;
+		}
+
+		else {
+			if (this.x < this.player.x) this.direction = 1;
+			else this.direction = -1;
+
+			if (Math.abs(this.player.x - this.x) > AGGRO_RANGE || Math.abs(this.player.x - this.x) < CLOSE_RANGE) {		// If out of aggro range
+				if (this.direction == 1) this.play("idleROgre", true);
+				else this.play("idleLOgre", true);
+				this.setVelocityX(0);
+			} else {
+				if (this.direction == 1) this.play("runROgre", true);
+				else this.play("runLOgre", true);
+				this.setVelocityX(SPEED * this.direction);
+
+				if (this.body.onFloor()) {
+					if ((this.direction == 1 && this.body.blocked.right) || (this.direction == -1 && this.body.blocked.left)) {
+						if (this.prevX == this.x) this.idle = true;		// if in front of too high wall
+						else {
+							this.setVelocityY(-JUMP_STRENGTH);
+							this.prevX = this.x;
+						}
 					}
 				}
 			}
